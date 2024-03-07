0ccfbf11e643b5341f73d276d92c0f3ca9c742c4
hotnAny <xiangchen@acm.org>
2024-03-06 20:20:58 -0800

    clean up background.js

--- scripts/background.js
+++
@@ -1,9 +0,0 @@
-// console.log("background.js")
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//     // Check if the tab's status is 'complete' which means the page and its resources are fully loaded
-//     if (changeInfo.status === 'complete') {
-//         // Send a message to the content script of this tab
-//         chrome.tabs.sendMessage(tabId, {"message": "cff on"});
-//     }
-// });
