df078caae7adb78b0ff9d6073922e7620d6e2b22
hotnAny <xiangchen@acm.org>
2024-08-26 20:14:13 -0700

    added data filtering by start and end dates

--- data/visualize.py
+++ data/visualize.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
+from datetime import datetime
 
 DATA_FILE = "morty_log_2024-08-24T03_31_04.319Z.json"
 
-# - # of mouse enter
+START_DATE = "08/21/2024"
+END_DATE = "08/31/2024"
+
+# - avg # of mouse enter
 
 if __name__ == "__main__":
-    # Open the JSON file
     with open(DATA_FILE, 'r') as file:
-        # Load the JSON data into a Python dictionary
         data = json.load(file)
 
-    # Print the data to ensure it's loaded correctly
-    print(data)
+    print(len(data))
+
+    start_date = datetime.strptime(START_DATE, "%m/%d/%Y")
+    end_date = datetime.strptime(END_DATE, "%m/%d/%Y")
+    for key in data:
+        # print(str_entry)
+        entry = data[key]
+        date_str = entry["timeStamp"]
+        date_entry = datetime.fromisoformat(date_str.replace("Z", "+00:00")).replace(tzinfo=None)
+        if start_date < date_entry and date_entry < end_date:
+            print(date_entry)
