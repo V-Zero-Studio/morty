1ded48078ef243841443c522bb1a5aee83ad7191
hotnAny <xiangchen@acm.org>
2024-08-28 20:26:52 -0700

    added simple analyses

--- data/visualize.py
+++ data/visualize.py
@@ -2,15 +2,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 from datetime import datetime
+import math
 
 DATA_FILE = "morty_log_2024-08-24T03_31_04.319Z.json"
 
 START_DATE = "08/21/2024"
 END_DATE = "08/31/2024"
 
+series_mouse_enter = []
+series_mouse_footprint = []
+
 cnt_sessions = 0
 cnt_mouse_enter = 0
 cnt_mouse_leave = 0
+sum_mouse_move = 0
+
+def calMouseFootprint(events):
+    footprint = 0
+    for i in range(1, len(events)):
+        dx = events[i]["coord"]["x"] - events[i-1]["coord"]["x"]
+        dy = events[i]["coord"]["y"] - events[i-1]["coord"]["y"]
+        footprint += math.sqrt(dx * dx + dy * dy)
+    return footprint
 
 if __name__ == "__main__":
     with open(DATA_FILE, 'r') as file:
@@ -31,9 +44,20 @@ if __name__ == "__main__":
         cnt_sessions += 1
 
         int_bev = data[key]["interactionBehaviors"]
+        stats = {}
 
         cnt_mouse_enter += len(int_bev["mouseenterEvents"])
+        series_mouse_enter.append(len(int_bev["mouseenterEvents"]))
+        
         cnt_mouse_leave += len(int_bev["mouseleaveEvents"])
 
-    print("avg mouse enter events:", cnt_mouse_enter / cnt_sessions)
-    print("avg mouse leave events:", cnt_mouse_leave / cnt_sessions)
+        footprint = calMouseFootprint(int_bev["mousemoveEvents"])
+        series_mouse_footprint.append(footprint)
+        sum_mouse_move += footprint
+
+    # print("avg mouse enter events:", cnt_mouse_enter / cnt_sessions)
+    # print("avg mouse leave events:", cnt_mouse_leave / cnt_sessions)
+    # print("avg mouse movement:", sum_mouse_move / cnt_sessions)
+
+    # print(series_mouse_enter)
+    # print(series_mouse_footprint)
