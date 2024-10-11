f9c0019f999eaa9ba615a15848b2b7a836938ee1
hotnAny <xiangchen@acm.org>
2024-10-10 20:13:56 -0700

    cleaned up

--- scripts/morty.js
+++ scripts/morty.js
@@ -77,15 +77,6 @@ const callbackNewResponse = (mutationsList, observer) => {
                     }
                     _observerNewResponse.disconnect()
 
-                    // if (elements.length == 1) {
-                        // starting a new prompt saves the previous session entry
-                        // this event listener needs to be (re)created every time the user
-                        // starts a new tab, which means # of response in history == 1
-                        // let elmPromptBox = document.getElementById(_config.ID_TEXTBOX_PROMPT)
-                        // elmPromptBox.removeEventListener("click", onClickPromptBox)
-                        // elmPromptBox.addEventListener("click", onClickPromptBox)
-                    // }
-
                     log("streaming starts")
                     _isStreaming = true
 
@@ -292,17 +283,6 @@ const revealResponse = () => {
     clearCffContainer(true)
     fadeIn(_elmResponse)
 }
-
-//
-//  event handler for clicking the prompt box to enter a prompt
-//
-// const onClickPromptBox = () => {
-    // if (_isLogging && _sessionEntry.timeStamp != undefined) {
-    //     saveLog()
-    //     clearTimeout(_autoSaveTimeout)
-    //     log("auto save timeout cleared")
-    // }
-// }
 
 //
 //  use a textarea's placeholder as a prefilled prefix for the text to be entered
