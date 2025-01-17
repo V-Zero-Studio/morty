340c18b4d9fd5938a09e1bafdbbae24e49919946
hotnAny <xiangchen@acm.org>
2025-01-16 20:06:19 -0800

    removed commented code

--- scripts/morty.js
+++ scripts/morty.js
@@ -5,50 +5,13 @@
 const PATH_CONFIG_FILE = "data/config.json";
 const PATH_POPUP_HTML = "scripts/popup.html"
 
-// design parameters for cff
-// const HEIGHT_CFF_CONTAINER = 100;
-// const HEIGHT_POST_RESPONSE = 300;
 const INTERVAL_MONITOR_STREAMING = 1000; // ms
-
-// design parameters for cff
-// const FADE_RATIO = 1.25; // how fast the covered response area fades
-// const FADE_OPACITY = 0.05; // the lowest opacity
-// const FADE_INTERVAL = 100; // smoothness of fading
-// const HTML_REVEAL_INFO = "(Click to reveal AI response)";
-
-// users' confidence levels
-// const CONFI_QUESTION_PROMPT =
-// "How confident are you if you were to respond to this prompt without ChatGPT's help?";
-// const CONFIDENCE_LEVELS = [
-//   "Not confident at all",
-//   "Slightly confident",
-//   "Moderately confident",
-//   "Quite confident",
-//   "Very confident",
-// ];
-
-// agreement rating
-// const AGREEMENT_QUESTION_PROMPT = "Do you agree with ChatGPT?";
-// const AGREEMENT_LEVELS = [
-//   "Totally disagree",
-//   "Somewhat disagree",
-//   "Have doubt",
-//   "Somewhat agree",
-//   "Totally agree",
-// ];
-// const TIMEOUT_PLACEHOLDER_RESET = 30000;
-// const STYLE_AGREEMENT_RATING = "rgba(255, 255, 0, 0.25)";
 
 let _on = true;
 let _isStreaming = false;
 let _config = {};
 let _observerNewResponse = undefined;
 let _elmResponse = undefined;
-// let _divCff = undefined;
-// let _promptCurrent = undefined;
-// let _placeholderPrompt = undefined;
-// let _divAgreementRating = undefined;
-// let _isFollowUp = false;
 
 // data logging
 const DT_EVENTS = 250;
@@ -60,37 +23,6 @@ let _isWindowBlur = false; // has the user left the window
 let _db;
 const ID_DB = "MortyDB";
 const ID_STORE = "sessionStore";
-
-//
-//  fade in the AI response area
-//
-// const fadeIn = (elm) => {
-//   if (elm == undefined) {
-//     return;
-//   }
-
-//   const opacity = parseFloat(elm.style.opacity);
-//   if (opacity < 1) {
-//     elm.style.opacity = (opacity * FADE_RATIO).toString();
-//     setTimeout(() => {
-//       fadeIn(elm);
-//     }, FADE_INTERVAL);
-//   }
-// };
-
-//
-// fade out and remove an element
-//
-// const fadeOutAndRemove = (element) => {
-//   // apply the fade-out class
-//   element.classList.add("fade-out");
-
-//   // listen for the end of the animation
-//   element.addEventListener("animationend", function () {
-//     element.classList.remove("fade-out");
-//     element.remove();
-//   });
-// };
 
 //
 //  a recurring function to monitor if streaming ends,
@@ -114,23 +46,6 @@ const monitorStreaming = () => {
       _sessionEntry.response.height =
         _elmResponse.getBoundingClientRect().height;
 
-      // if (_on) {
-      //   // if the cff container has not been clear, don't set up post response yet;
-      //   // instead, set it up when a user clicks to reveal response
-      //   if (document.getElementsByClassName("cff-container").length <= 0) {
-      //     setupPostResponseElements();
-      //   } else {
-      //     if (_elmResponse.parentElement != null) {
-      //       _elmResponse.parentElement.addEventListener(
-      //         "click",
-      //         setupPostResponseElements
-      //       );
-      //     }
-      //   }
-
-      //   _isFollowUp = false;
-      // }
-
       // auto save when the user is not focused on the current window
       if (_isWindowBlur) {
         _autoSaveTimeout = setTimeout(() => {
@@ -140,177 +55,6 @@ const monitorStreaming = () => {
     }
   }, INTERVAL_MONITOR_STREAMING);
 };
-
-//
-// remove children in the container and remove the container
-//
-// const clearCffContainer = (fadeOut = true) => {
-//   if (fadeOut) {
-//     fadeOutAndRemove(_divCff);
-//   } else {
-//     _divCff.remove();
-//   }
-
-//   if (_elmResponse.parentElement != null) {
-//     _elmResponse.parentElement.removeEventListener("click", revealResponse);
-//   }
-// };
-
-//
-// set up the cff elements
-//
-// const setupCffElements = () => {
-//   _elmResponse.style.opacity = FADE_OPACITY.toString();
-//   clearCffContainer(false);
-//   _elmResponse.parentElement.appendChild(_divCff);
-//   _elmResponse.parentElement.addEventListener("click", revealResponse);
-
-//   const spanRevealInfo = document.createElement("span");
-//   spanRevealInfo.classList.add("reveal");
-//   spanRevealInfo.innerHTML = HTML_REVEAL_INFO;
-//   _divCff.appendChild(spanRevealInfo);
-// };
-
-//
-//  set up ui to specify rating
-//  row: whether to place everything in a row (o/w in two lines: one for question and one for rating scale)
-//  onRated: call back when rating changes (by mouse hovering)
-//
-// const setupRatingUI = (
-//   id,
-//   question,
-//   labelsRating,
-//   row = false,
-//   onRated = undefined
-// ) => {
-//   const divRating = document.createElement("div");
-
-//   if (row) {
-//     divRating.classList.add("rating-row");
-//   }
-
-//   // the question
-//   const pRatingQuestion = document.createElement("p");
-//   pRatingQuestion.innerHTML = question;
-//   pRatingQuestion.classList.add("rating-question");
-//   divRating.appendChild(pRatingQuestion);
-
-//   // the rating options
-//   const divDots = document.createElement("div");
-//   divDots.classList.add("dots");
-
-//   for (let i = 0; i < labelsRating.length; i++) {
-//     const spanDot = document.createElement("span");
-//     spanDot.classList.add("dot");
-//     spanDot.setAttribute("name", id + "-dot");
-//     spanDot.addEventListener("mouseover", (e) => {
-//       document.getElementById(id + "-span").innerHTML = labelsRating[i];
-
-//       const dots = document.getElementsByName(id + "-dot");
-//       for (let j = 0; j < dots.length; j++) {
-//         if (j <= i) {
-//           dots[j].classList.add("selected");
-//         } else {
-//           dots[j].classList.remove("selected");
-//         }
-//       }
-
-//       if (onRated) {
-//         onRated(i);
-//       }
-//     });
-
-//     divDots.appendChild(spanDot);
-//   }
-
-//   // the label that describes rating
-//   const spanRating = document.createElement("span");
-//   spanRating.setAttribute("id", id + "-span");
-//   spanRating.classList.add("rating");
-//   divDots.appendChild(spanRating);
-
-//   divRating.appendChild(divDots);
-
-//   return divRating;
-// };
-
-//
-// reveal ai response
-//
-// const revealResponse = () => {
-//   clearCffContainer(true);
-//   fadeIn(_elmResponse);
-// };
-
-//
-//  use a textarea's placeholder as a prefilled prefix for the text to be entered
-//
-// const prefixPrompt = (e) => {
-//   e.target.value = e.target.getAttribute("placeholder") + " ";
-//   const textLength = e.target.value.length;
-//   e.target.setSelectionRange(textLength, textLength);
-
-//   // only do this prefixing once
-//   //   e.target.removeEventListener("click", prefixPrompt);
-// };
-
-//
-//  set up post response ui elements for mitigation
-//
-// const setupPostResponseElements = () => {
-//   const toolbar = document.querySelectorAll(_config.QUERY_TOOLBAR)[0];
-//   toolbar.appendChild(_divAgreementRating);
-
-//   document
-//     .getElementsByName("labelAgreement" + "-dot")
-//     .forEach((elm) => elm.classList.remove("selected"));
-//   const label = document.getElementById("labelAgreement" + "-span");
-//   if (label != null) {
-//     label.innerHTML = "";
-//   }
-
-//   // in case this is triggered by clicking the reveal response option
-//   // enable such handler only once
-//   _elmResponse.parentElement.removeEventListener(
-//     "click",
-//     setupPostResponseElements
-//   );
-// };
-
-//
-//  use a simple rule to detect if the prompt is a follow-up based on disagreement
-//
-// const isFollowUp = (prompt) => {
-//   for (idx in AGREEMENT_LEVELS) {
-//     if (idx <= AGREEMENT_LEVELS.length / 2) {
-//       if (prompt.includes("I " + AGREEMENT_LEVELS[idx].toLowerCase())) {
-//         return true;
-//       }
-//     }
-//   }
-
-//   return false;
-// };
-
-//
-// configure cff: start or stop the monitor for implementing cff on the response element
-//
-// const configCff = () => {
-//   // create a container for added cff elements
-//   _divCff = document.createElement("div");
-//   _divCff.classList.add("cff-container");
-
-//   const isDarkMode =
-//     document.documentElement.getAttribute("class").indexOf("dark") > -1;
-//   _divCff.classList.add(isDarkMode ? "dark" : "light");
-
-//   // position the cff container at a fixed position above the prompt input box
-//   let elmPromptBox = document.getElementById(_config.ID_TEXTBOX_PROMPT);
-//   let rect = elmPromptBox.getBoundingClientRect();
-//   let topPosition = rect.top + window.scrollY;
-//   _divCff.style.height = `${HEIGHT_CFF_CONTAINER}px`;
-//   _divCff.style.top = `${topPosition - HEIGHT_CFF_CONTAINER}px`;
-// };
 
 //
 //  start the routine of monitoring streaming
@@ -346,29 +90,8 @@ const startMonitoring = () => {
 
     monitorStreaming();
 
-    // reset all previous response elements to full opacity
-    // elements.forEach((value, index, array) => {
-    //   array[index].style.opacity = 1;
-    // });
-
     logInteractionBehaviorOnResponse();
 
-    // _sessionEntry.prompt.isFollowUp = _isFollowUp;
-    // if (_on && !_isFollowUp) {
-    //   setupCffElements();
-    //   const divRating = setupRatingUI(
-    //     "labelConfidence",
-    //     CONFI_QUESTION_PROMPT,
-    //     CONFIDENCE_LEVELS,
-    //     false,
-    //     (idxRating) => {
-    //       // data logging
-    //       _sessionEntry.confidenceRating.rating = idxRating;
-    //       _sessionEntry.confidenceRating.timeStamp = time();
-    //     }
-    //   );
-    //   _divCff.prepend(divRating);
-    // }
   } else {
     setTimeout(() => {
       startMonitoring();
@@ -433,11 +156,6 @@ const init = () => {
 
         if (event.key === "Enter" && !event.shiftKey) {
           const prompt = event.target.innerText;
-          // _isFollowUp = isFollowUp(prompt);
-
-          // if (_on) {
-          //   configCff();
-          // }
 
           // data logging - saving previous session
           if (_isLogging && _sessionEntry != undefined) {
@@ -471,10 +189,6 @@ const init = () => {
     // currently svg can capture this button press but maybe also svg's
     // but with false positives, configCff wouldn't cause any subsequent actions
     if (event.target.tagName === "svg") {
-      // if (_on) {
-      //   configCff();
-      // }
-
       // data logging
       _sessionEntry = createNewLogEntry();
       _sessionEntry.prompt.text = _promptCurrent;
@@ -491,11 +205,6 @@ const init = () => {
   btnSwitch.classList.add("switch");
   btnSwitch.style.filter = _on ? "" : "grayscale(100%)";
   btnSwitch.addEventListener("click", (e) => {
-    // disabled for data logging under on/off conditions
-    // todo: put back later when needed
-    // _on = !_on
-    // btnSwitch.style.filter = _on ? '' : 'grayscale(100%)'
-
     if (popup.style.display === 'none') {
       popup.style.display = 'block';
     } else {
@@ -513,7 +222,6 @@ const init = () => {
   const popup = document.createElement("div")
   popup.classList.add("mini-popup")
   popup.style.display = "none" // Initially hidden
-  // popup.innerHTML = "Hello"
   document.body.appendChild(popup)
 
   // Fetch the HTML file
@@ -535,104 +243,6 @@ const init = () => {
     log(error.message)
   });
 
-  // extract the default placeholder in the prompt box
-  // let elmPromptBox = document.getElementById(_config.ID_TEXTBOX_PROMPT);
-  // if (elmPromptBox != null) {
-  //   _placeholderPrompt = elmPromptBox.getAttribute("placeholder");
-  // } else {
-  //   console.error("[morty]", "unable to locate prompt box!");
-  //   alert("Error initiating MORTY. Please refresh this page.");
-  // }
-
-  // set up the disagreement rating ui (just once)
-  // _divAgreementRating = setupRatingUI(
-  //   "labelAgreement",
-  //   AGREEMENT_QUESTION_PROMPT,
-  //   AGREEMENT_LEVELS,
-  //   true,
-  //   (idxRating) => {
-  //     let elmPromptBox = document.getElementById(_config.ID_TEXTBOX_PROMPT);
-  //     const ratingNormalized = (idxRating * 1.0) / AGREEMENT_LEVELS.length;
-  //     const placeholder =
-  //       "I " + AGREEMENT_LEVELS[idxRating].toLowerCase() + " because";
-  //     if (ratingNormalized < 0.5) {
-  //       elmPromptBox.setAttribute("placeholder", placeholder);
-  //       // NOTE: as of 12/2024, we could no longer change the placeholder
-  //       // there is some open ai code that keeps setting the placeholder back to its default value
-  //       // let elmPromptPlaceHolder = document.querySelector(_config.QUERY_PLACEHOLDER)
-  //       // elmPromptPlaceHolder.setAttribute("data-placeholder", placeholder)
-
-  //       // clicking the prompt box will use the placeholder as the prefix to prefill the prompt
-  //       // elmPromptBox.addEventListener("click", prefixPrompt);
-
-  //       // remove it after a timeout, assuming the user would have ignored it by then
-  //       setTimeout(() => {
-  //         //   elmPromptBox.removeEventListener("click", prefixPrompt);
-  //         elmPromptBox.setAttribute("placeholder", _placeholderPrompt);
-  //       }, TIMEOUT_PLACEHOLDER_RESET);
-  //     } else {
-  //       elmPromptBox.setAttribute("placeholder", _placeholderPrompt);
-  //       // elmPromptBox.removeEventListener("click", prefixPrompt);
-  //     }
-
-  //     // data logging
-  //     if (_sessionEntry != undefined) {
-  //       _sessionEntry.agreementRating.rating = idxRating;
-  //       _sessionEntry.agreementRating.timeStamp = time();
-  //     }
-  //   }
-  // );
-
-  // _divAgreementRating.style.background = STYLE_AGREEMENT_RATING;
-
-  // // remove the agreement rating when finished, providing a closure
-  // _divAgreementRating
-  //   .querySelectorAll('[name="labelAgreement-dot"]')
-  //   .forEach((elm) => {
-  //     elm.addEventListener("click", () => {
-  //       fadeOutAndRemove(_divAgreementRating);
-  //     });
-  //   });
-
-  // house keeping when starting a new chat
-  // let btnNewChat = document.querySelectorAll(_config.QUERY_NEWCHAT)[0];
-  // btnNewChat.addEventListener("click", () => {
-  //   saveLog();
-  // });
-
-  // adding one-time window-switching monitoring for logging
-  // window.addEventListener("blur", (e) => {
-  //   if (_sessionEntry == undefined) {
-  //     return
-  //   }
-
-  //   _sessionEntry.interactionBehaviors.windowleaveEvents.push({
-  //     timeStamp: time(),
-  //   });
-  //   _isWindowBlur = false;
-  //   // if the user leaves the page during streaming, we assume they are not done with the session
-  //   // so we don't end and save the log entry
-  //   if (!_isStreaming && _autoSaveTimeout == undefined) {
-  //     _autoSaveTimeout = setTimeout(() => {
-  //       saveLog();
-  //       // in case the user wasn't engaged
-  //       // fadeOutAndRemove(_divAgreementRating);
-  //       _autoSaveTimeout = undefined;
-  //     }, TIMEOUT_AUTO_LOG_SAVE);
-  //     log("auto save timeout started");
-  //   }
-  // });
-
-  // window.addEventListener("focus", (e) => {
-  //   if (_sessionEntry == undefined) return;
-  //   _sessionEntry.interactionBehaviors.windowenterEvents.push({
-  //     timeStamp: time(),
-  //   });
-  //   _isWindowBlur = true;
-  //   clearTimeout(_autoSaveTimeout);
-  //   log("auto save timeout cleared");
-  //   _autoSaveTimeout = undefined;
-  // });
 
   document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "hidden") {
@@ -697,15 +307,9 @@ const createNewLogEntry = () => {
   return {
     timeStamp: time(),
     prompt: {
-      // timeStart: undefined,
       timeSent: undefined,
       text: undefined,
-      // isFollowUp: undefined,
     },
-    // confidenceRating: {
-    //   timeStamp: undefined, // the time of the last-hovered rating
-    //   rating: undefined,
-    // },
     response: {
       timeStreamingStarted: undefined,
       timeStreamingEnded: undefined,
