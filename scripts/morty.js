b032461282f8054b29f497c2e6d9f125bb2a0b29
hotnAny <xiangchen@acm.org>
2025-02-11 20:16:29 -0800

    cleaned up

--- scripts/morty.js
+++ scripts/morty.js
@@ -329,27 +329,13 @@ const aggregateSeries = () => {
       }
 
       plot(mapDailyStats)
-
-      // log(mapDailyStats)
-
-      // const minDate = new Date(
-      //   Math.min(...series_timeStamps.map((date) => new Date(date).getTime()))
-      // );
-      // const maxDate = new Date(
-      //   Math.max(...series_timeStamps.map((date) => new Date(date).getTime()))
-      // );
-      // const days = ((maxDate - minDate) / (1000 * 60 * 60 * 24) | 0);
-      // log("avg # of sessions per day: " + (cnt_sessions / days))
-
-      // const dates = [...mapDailyStats.keys()].sort((a, b) =>
-      //   a.localeCompare(b)
-      // );
-      // const minDate = dates[0]; // First date in sorted order
-      // const maxDate = dates[dates.length - 1]; // Last date in sorted order
     });
   });
 };
 
+//
+//
+//
 const plot = (dataMap) => {
   const data = Array.from(dataMap, ([date, value]) => ({
     date: new Date(date), // Convert to Date object
@@ -397,81 +383,6 @@ const plot = (dataMap) => {
     .attr("stroke", "steelblue")
     .attr("stroke-width", 2)
     .attr("d", line);
-};
-
-//
-const testChartJs = () => {
-  // set the dimensions and margins of the graph
-  const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-  // append the svg object to the body of the page
-  const svg = d3
-    .select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-  //Read the data
-  d3.csv(
-    "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-
-    // When reading the csv, I must format variables:
-    function (d) {
-      return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value };
-    }
-  ).then(
-    // Now I can use this dataset:
-    function (data) {
-      // Add X axis --> it is a date format
-      const x = d3
-        .scaleTime()
-        .domain(
-          d3.extent(data, function (d) {
-            return d.date;
-          })
-        )
-        .range([0, width]);
-      svg
-        .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-
-      // Add Y axis
-      const y = d3
-        .scaleLinear()
-        .domain([
-          0,
-          d3.max(data, function (d) {
-            return +d.value;
-          }),
-        ])
-        .range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
-
-      // Add the line
-      svg
-        .append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr(
-          "d",
-          d3
-            .line()
-            .x(function (d) {
-              return x(d.date);
-            })
-            .y(function (d) {
-              return y(d.value);
-            })
-        );
-    }
-  );
 };
 
 //
