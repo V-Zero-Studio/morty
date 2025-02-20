051ca2191db248281ef43ccdc24ad4f1ed92f94e
hotnAny <xiangchen@acm.org>
2025-02-19 20:01:59 -0800

    minor fixes

--- scripts/morty.js
+++ scripts/morty.js
@@ -2,28 +2,28 @@
 // MORTY: MAIN
 //
 
-const PATH_CONFIG_FILE = "data/config.json";
-const PATH_POPUP_HTML = "scripts/popup.html";
-const PATH_VIS_LIB = "scripts/chart.js";
+const PATH_CONFIG_FILE = "data/config.json"
+const PATH_POPUP_HTML = "scripts/popup.html"
+const PATH_VIS_LIB = "scripts/chart.js"
 
-const INTERVAL_MONITOR_STREAMING = 1000; // ms
+const INTERVAL_MONITOR_STREAMING = 1000 // ms
 
-let _on = true;
-let _isStreaming = false;
-let _config = {};
-let _observerNewResponse = undefined;
-let _elmResponse = undefined;
+let _on = true
+let _isStreaming = false
+let _config = {}
+let _observerNewResponse = undefined
+let _elmResponse = undefined
 
 // data logging
-const DT_EVENTS = 250;
-const TIMEOUT_AUTO_LOG_SAVE = 30000;
-let _isLogging = true;
-let _sessionEntry;
-let _autoSaveTimeout;
-let _isWindowBlur = false; // has the user left the window
-let _db;
-const ID_DB = "MortyDB";
-const ID_STORE = "sessionStore";
+const DT_EVENTS = 250
+const TIMEOUT_AUTO_LOG_SAVE = 30000
+let _isLogging = true
+let _sessionEntry
+let _autoSaveTimeout
+let _isWindowBlur = false // has the user left the window
+let _db
+const ID_DB = "MortyDB"
+const ID_STORE = "sessionStore"
 
 //
 //  a recurring function to monitor if streaming ends,
@@ -34,70 +34,70 @@ const monitorStreaming = () => {
     // indicator of streaming ended
     let elements = document.querySelectorAll(
       '[class*="' + _config.KEYWORD_STREAMING + '"]'
-    );
+    )
     if (elements.length > 0) {
-      monitorStreaming();
-      log("monitoring streaming ...");
+      monitorStreaming()
+      log("monitoring streaming ...")
     } else {
-      log("streaming ends");
-      _isStreaming = false;
+      log("streaming ends")
+      _isStreaming = false
 
       // data logging
-      _sessionEntry.response.timeStreamingEnded = time();
+      _sessionEntry.response.timeStreamingEnded = time()
       _sessionEntry.response.height =
-        _elmResponse.getBoundingClientRect().height;
+        _elmResponse.getBoundingClientRect().height
 
       // auto save when the user is not focused on the current window
       if (_isWindowBlur) {
         _autoSaveTimeout = setTimeout(() => {
-          saveLog();
-        }, TIMEOUT_AUTO_LOG_SAVE);
+          saveLog()
+        }, TIMEOUT_AUTO_LOG_SAVE)
       }
     }
-  }, INTERVAL_MONITOR_STREAMING);
-};
+  }, INTERVAL_MONITOR_STREAMING)
+}
 
 //
 //  start the routine of monitoring streaming
 //
 const startMonitoring = () => {
   if (_isStreaming) {
-    return;
+    return
   }
 
-  log("looking for streaming div ...");
+  log("looking for streaming div ...")
   let divStreaming = document.querySelector(
     "[class*='" + _config.KEYWORD_STREAMING + "']"
-  );
+  )
 
   if (divStreaming != null) {
-    log("found the streaming div!");
-    let elements = document.querySelectorAll(_config.QUERY_ELM_RESPONSE);
-    _elmResponse = elements[elements.length - 1];
+    log("found the streaming div!")
+    let elements = document.querySelectorAll(_config.QUERY_ELM_RESPONSE)
+    _elmResponse = elements[elements.length - 1]
 
     // keep monitoring until the actual response arrives
     if (_elmResponse.textContent.length == 0) {
-      return;
+      return
     }
 
-    log("streaming starts");
-    _isStreaming = true;
+    log("streaming starts")
+    _isStreaming = true
 
     // data logging
-    _sessionEntry.timeStamp = time();
-    _sessionEntry.on = _on;
-    _sessionEntry.response.timeStreamingStarted = time();
-    _sessionEntry.viewHeight = window.innerHeight;
+    _sessionEntry.timeStamp = time()
+    _sessionEntry.on = _on
+    _sessionEntry.response.timeStreamingStarted = time()
+    _sessionEntry.viewHeight = window.innerHeight
 
-    monitorStreaming();
+    monitorStreaming()
 
-    logInteractionBehaviorOn(_elmResponse, _sessionEntry);
+    logInteractionBehaviorOn(_elmResponse, _sessionEntry)
   } else {
     setTimeout(() => {
-      startMonitoring();
-    }, 1000);
+      startMonitoring()
+    }, 1000)
   }
-};
+}
 
 //
 //  trigger a dialog to download an object as a json file
@@ -105,73 +105,73 @@ const startMonitoring = () => {
 const downloadObjectAsJson = (exportObj, exportName) => {
   // convert the object to a JSON string
   const dataStr =
-    "data:text/json;charset=utf-8," +
-    encodeURIComponent(JSON.stringify(exportObj));
+    "data:text/jsoncharset=utf-8," +
+    encodeURIComponent(JSON.stringify(exportObj))
 
   // create an invisible anchor element
-  const downloadAnchorNode = document.createElement("a");
+  const downloadAnchorNode = document.createElement("a")
 
   // set the download attribute with a filename
-  downloadAnchorNode.setAttribute("href", dataStr);
-  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  downloadAnchorNode.setAttribute("href", dataStr)
+  downloadAnchorNode.setAttribute("download", exportName + ".json")
 
   // append the anchor to the document, trigger a click on it, and then remove it
-  document.body.appendChild(downloadAnchorNode);
-  downloadAnchorNode.click();
-  downloadAnchorNode.remove();
-};
+  document.body.appendChild(downloadAnchorNode)
+  downloadAnchorNode.click()
+  downloadAnchorNode.remove()
+}
 
 //
 // initialize pop-up ui (on the web page)
 //
 const initPopupUI = () => {
-  const buttons = document.querySelectorAll(".tab-btn");
-  const contents = document.querySelectorAll(".tab-content");
+  const buttons = document.querySelectorAll(".tab-btn")
+  const contents = document.querySelectorAll(".tab-content")
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       // Remove active class from all buttons and content
-      buttons.forEach((btn) => btn.classList.remove("active"));
-      contents.forEach((content) => content.classList.remove("active"));
+      buttons.forEach((btn) => btn.classList.remove("active"))
+      contents.forEach((content) => content.classList.remove("active"))
 
       // Add active class to the clicked button and corresponding content
-      button.classList.add("active");
+      button.classList.add("active")
       document
         .getElementById(button.getAttribute("data-tab"))
-        .classList.add("active");
-    });
-  });
-};
+        .classList.add("active")
+    })
+  })
+}
 
 //
 // initialization
 //
 const init = () => {
-  log("ready");
+  log("ready")
 
   // trigger mitigation from enter key press to send prompt
   document.addEventListener(
     "keydown",
     async (event) => {
       if (event.target.id === _config.ID_PROMPT_INPUT) {
-        _promptCurrent = event.target.innerText;
+        _promptCurrent = event.target.innerText
 
         if (event.key === "Enter" && !event.shiftKey) {
-          const prompt = event.target.innerText;
+          const prompt = event.target.innerText
 
           // data logging - saving previous session
           if (_isLogging && _sessionEntry != undefined) {
-            await saveLog();
-            clearTimeout(_autoSaveTimeout);
-            log("auto save timeout cleared");
+            await saveLog()
+            clearTimeout(_autoSaveTimeout)
+            log("auto save timeout cleared")
           }
 
-          _sessionEntry = createNewLogEntry();
-          _sessionEntry.prompt.text = _promptCurrent;
-          _sessionEntry.prompt.timeSent = time();
-          log("prompt logged");
+          _sessionEntry = createNewLogEntry()
+          _sessionEntry.prompt.text = _promptCurrent
+          _sessionEntry.prompt.timeSent = time()
+          log("prompt logged")
 
-          startMonitoring();
+          startMonitoring()
         }
       } else {
         // data logging
@@ -179,12 +179,12 @@ const init = () => {
           _sessionEntry.interactionBehaviors.keydownEvents.push({
             timeStamp: time(),
             key: event.key,
-          });
+          })
         }
       }
     },
     true
-  );
+  )
 
   // trigger mitigation from pressing send button
   document.addEventListener("click", function (event) {
@@ -192,96 +192,95 @@ const init = () => {
     // but with false positives, configCff wouldn't cause any subsequent actions
     if (event.target.tagName === "svg") {
       // data logging
-      _sessionEntry = createNewLogEntry();
-      _sessionEntry.prompt.text = _promptCurrent;
-      _sessionEntry.prompt.timeSent = time();
+      _sessionEntry = createNewLogEntry()
+      _sessionEntry.prompt.text = _promptCurrent
+      _sessionEntry.prompt.timeSent = time()
 
-      startMonitoring();
+      startMonitoring()
     }
-  });
+  })
 
   // create on-web-page ui
-  const btnSwitch = document.createElement("img");
-  btnSwitch.src = chrome.runtime.getURL(_config.URL_ICON);
-  btnSwitch.alt = "Toggle Button";
-  btnSwitch.classList.add("icon-button");
-  btnSwitch.style.filter = _on ? "" : "grayscale(100%)";
+  const btnSwitch = document.createElement("img")
+  btnSwitch.src = chrome.runtime.getURL(_config.URL_ICON)
+  btnSwitch.alt = "Toggle Button"
+  btnSwitch.classList.add("icon-button")
+  btnSwitch.style.filter = _on ? "" : "grayscale(100%)"
   btnSwitch.addEventListener("click", (e) => {
+    let containerVis = document.getElementById("tabVis")
     if (popup.style.display === "none") {
-      popup.style.display = "block";
-
-      let containerVis = document.getElementById("tabVis");
-      visualizeSeries(containerVis);
-      // testChartJs();
+      popup.style.display = "block"
+      visualizeSeries(containerVis)
     } else {
-      popup.style.display = "none";
+      popup.style.display = "none"
+      containerVis.innerHTML = ""
     }
-  });
+  })
   // btnSwitch.addEventListener("dblclick", () => {
   //   readFromDB((logData) => {
-  //     downloadObjectAsJson(logData, "morty_log_" + time().replace(":", "_"));
-  //   });
-  // });
-  document.body.appendChild(btnSwitch);
+  //     downloadObjectAsJson(logData, "morty_log_" + time().replace(":", "_"))
+  //   })
+  // })
+  document.body.appendChild(btnSwitch)
 
   // create on-web-page mini page
-  const popup = document.createElement("div");
-  popup.classList.add("mini-popup");
-  popup.style.display = "none"; // Initially hidden
-  document.body.appendChild(popup);
+  const popup = document.createElement("div")
+  popup.classList.add("mini-popup")
+  popup.style.display = "none" // Initially hidden
+  document.body.appendChild(popup)
 
   // fetch the HTML file
-  const popupHtmlPath = chrome.runtime.getURL(PATH_POPUP_HTML);
+  const popupHtmlPath = chrome.runtime.getURL(PATH_POPUP_HTML)
   fetch(popupHtmlPath)
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`Failed to load HTML file: ${response.statusText}`);
+        throw new Error(`Failed to load HTML file: ${response.statusText}`)
       }
-      return response.text();
+      return response.text()
     })
     .then((htmlContent) => {
       // inject the HTML content into the target element
-      popup.innerHTML = htmlContent;
-      initPopupUI();
+      popup.innerHTML = htmlContent
+      initPopupUI()
     })
     .catch((error) => {
       // handle errors
-      log(error.message);
-    });
+      log(error.message)
+    })
 
   document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "hidden") {
       if (_sessionEntry == undefined) {
-        return;
+        return
       }
 
       _sessionEntry.interactionBehaviors.windowleaveEvents.push({
         timeStamp: time(),
-      });
-      _isWindowBlur = false;
+      })
+      _isWindowBlur = false
       // if the user leaves the page during streaming, we assume they are not done with the session
       // so we don't end and save the log entry
       if (!_isStreaming && _autoSaveTimeout == undefined) {
         _autoSaveTimeout = setTimeout(() => {
-          saveLog();
+          saveLog()
           // in case the user wasn't engaged
-          // fadeOutAndRemove(_divAgreementRating);
-          _autoSaveTimeout = undefined;
-        }, TIMEOUT_AUTO_LOG_SAVE);
-        log("auto save timeout started");
+          // fadeOutAndRemove(_divAgreementRating)
+          _autoSaveTimeout = undefined
+        }, TIMEOUT_AUTO_LOG_SAVE)
+        log("auto save timeout started")
       }
     } else if (document.visibilityState === "visible") {
-      if (_sessionEntry == undefined) return;
+      if (_sessionEntry == undefined) return
       _sessionEntry.interactionBehaviors.windowenterEvents.push({
         timeStamp: time(),
-      });
-      _isWindowBlur = true;
-      clearTimeout(_autoSaveTimeout);
-      log("auto save timeout cleared");
-      _autoSaveTimeout = undefined;
+      })
+      _isWindowBlur = true
+      clearTimeout(_autoSaveTimeout)
+      log("auto save timeout cleared")
+      _autoSaveTimeout = undefined
     }
-  });
-};
+  })
+}
 
 //
 //  save the latest session's log entry
@@ -292,60 +291,60 @@ const saveLog = async () => {
     _sessionEntry.timeStamp == undefined ||
     _sessionEntry.prompt.text == undefined
   ) {
-    return;
+    return
   }
 
   return new Promise((resolve, reject) => {
     writeToDB(_sessionEntry, () => {
-      log("data successfully stored.");
-      log(_sessionEntry);
-      _sessionEntry = undefined;
-      resolve();
-    });
-  });
-};
+      log("data successfully stored.")
+      log(_sessionEntry)
+      _sessionEntry = undefined
+      resolve()
+    })
+  })
+}
 
 //
 //  shortcut method to get the current time as a string
 //
 const time = () => {
-  return new Date().toISOString();
-};
+  return new Date().toISOString()
+}
 
 //
 //  a short cut to do console.log
 //
 const log = (msg) => {
-  console.info("[morty]", time(), msg);
-};
+  console.info("[morty]", time(), msg)
+}
 
 //
 //  entry function
 //
 (function () {
-  const jsonFilePath = chrome.runtime.getURL(PATH_CONFIG_FILE);
+  const jsonFilePath = chrome.runtime.getURL(PATH_CONFIG_FILE)
 
   // load _config file
   window.addEventListener("load", function () {
     fetch(jsonFilePath)
       .then((response) => response.json())
       .then((data) => {
-        _config = data;
+        _config = data
 
-        init();
+        init()
 
         // DANGER! KEEP IT COMMENTED
-        // indexedDB.deleteDatabase(ID_DB);
+        // indexedDB.deleteDatabase(ID_DB)
 
-        openDB(readFromDB);
+        openDB(readFromDB)
 
-        _sessionEntry = createNewLogEntry();
+        _sessionEntry = createNewLogEntry()
       })
-      .catch((error) => console.error("Error fetching JSON:", error));
-  });
+      .catch((error) => console.error("Error fetching JSON:", error))
+  })
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    log("from popup: " + request);
-    sendResponse({ status: "message logged" });
-  });
-})();
+    log("from popup: " + request)
+    sendResponse({ status: "message logged" })
+  })
+})()
--- scripts/va.js
+++ scripts/va.js
@@ -3,67 +3,67 @@
 //
 
 //
-//
+//  aggregate multiple series of log data and visualize it
 //
 const visualizeSeries = (containerVis) => {
   openDB((event) => {
     readFromDB((series) => {
-      const series_timeStamps = [];
-      const mapDailyStats = new Map();
-      let cnt_sessions = 0;
+      const series_timeStamps = []
+      const mapDailyStats = new Map()
+      let cnt_sessions = 0
 
       // preprocesing log for visualization
       for (const entry of series) {
         // log(entry)
-        cnt_sessions += 1;
-        series_timeStamps.push(entry.timeStamp);
+        cnt_sessions += 1
+        series_timeStamps.push(entry.timeStamp)
 
         // update a map of daily stats
-        const strDate = entry.timeStamp.split("T")[0];
-        let numSessions = 0;
+        const strDate = entry.timeStamp.split("T")[0]
+        let numSessions = 0
         if (mapDailyStats.has(strDate)) {
-          numSessions = mapDailyStats.get(strDate);
+          numSessions = mapDailyStats.get(strDate)
         }
-        mapDailyStats.set(strDate, numSessions + 1);
+        mapDailyStats.set(strDate, numSessions + 1)
       }
 
-      plot(mapDailyStats, createDivVis("visDailyStats", containerVis));
-    });
-  });
-};
+      plot(mapDailyStats, createDivVis("visDailyStats", containerVis))
+    })
+  })
+}
 
 const createDivVis = (id, container) => {
-  let divVis = document.createElement("div");
-  divVis.id = id;
-  container.appendChild(divVis);
-  return id;
-};
+  let divVis = document.createElement("div")
+  divVis.id = id
+  container.appendChild(divVis)
+  return id
+}
 
 //
-//
+//  plotting data series in a div
 //
 const plot = (dataMap, idDivVis) => {
   const data = Array.from(dataMap, ([date, value]) => ({
     date: new Date(date), // Convert to Date object
     value,
-  }));
+  }))
 
-  const margin = { top: 20, right: 30, bottom: 40, left: 50 };
-  const width = 600 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  const margin = { top: 20, right: 30, bottom: 40, left: 50 }
+  const width = 600 - margin.left - margin.right
+  const height = 400 - margin.top - margin.bottom
 
   const x = d3
     .scaleTime()
     .domain(d3.extent(data, (d) => d.date)) // Get min and max dates
-    .range([0, width]);
+    .range([0, width])
 
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.value)]) // Scale to max value
-    .range([height, 0]);
+    .range([height, 0])
 
-  const xAxis = d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %d"));
-  const yAxis = d3.axisLeft(y);
+  const xAxis = d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %d"))
+  const yAxis = d3.axisLeft(y)
 
   const svg = d3
     .select("#" + idDivVis)
@@ -71,16 +71,16 @@ const plot = (dataMap, idDivVis) => {
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${margin.top})`)
 
-  svg.append("g").attr("transform", `translate(0,${height})`).call(xAxis);
+  svg.append("g").attr("transform", `translate(0,${height})`).call(xAxis)
 
-  svg.append("g").call(yAxis);
+  svg.append("g").call(yAxis)
 
   const line = d3
     .line()
     .x((d) => x(d.date))
-    .y((d) => y(d.value));
+    .y((d) => y(d.value))
 
   svg
     .append("path")
@@ -88,5 +88,5 @@ const plot = (dataMap, idDivVis) => {
     .attr("fill", "none")
     .attr("stroke", "steelblue")
     .attr("stroke-width", 2)
-    .attr("d", line);
-};
+    .attr("d", line)
+}
